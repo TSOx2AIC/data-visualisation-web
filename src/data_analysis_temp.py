dcac61d08530f4f9727a4502e00201e9dee566f9
Aleksandra Ma <tengma@berkeley.edu>
2022-07-10 02:36:03 -0400

    add user-based 3d plots

--- src/data_analysis_temp.py
+++ src/data_analysis_temp.py
@@ -5,15 +5,20 @@ import pandas as pd
 def load_data():
     user_data = {}
     top_50 = pd.DataFrame()
+
+    #entries = os.listdir("data/clean")
     
-    entries = os.listdir("/data/clean")
+    entries = os.listdir("../data/clean")
+    entries.remove('.DS_Store')
     for entry in entries:
         # Load user info
-        with open("data/clean/" + entry + "/userinfo.json") as f:
+        # with open("data/clean/" + entry + "/userinfo.json") as f:
+        with open("../data/clean/" + entry + "/userinfo.json") as f:
             user_data[entry] = json.load(f)
         
         # Load top 50 data
-        df = pd.read_csv("data/clean/" + entry + "/top50.csv")
+        #df = pd.read_csv("data/clean/" + entry + "/top50.csv")
+        df = pd.read_csv("../data/clean/" + entry +  "/top50.csv")
         df["user_id"] = user_data[entry]["id"]
         top_50 = top_50.append(df, ignore_index=True)
     return user_data, top_50
